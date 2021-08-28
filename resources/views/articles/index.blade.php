a39b9ddf0eae10856479e09e17f137ebbe2155d1
ts44gj <ts44gj2020@gmail.com>
2021-08-29 01:13:04 +0900

    modal

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -25,8 +25,44 @@
                                         <i class="fas fa-ellipsis-v"></i>
                                     </button>
                                 </a>
+                                <div class='dropdown-menu dropdown-menu-right'>
+                                    <a class="dropdown-item" href="{{ route('articles.edit', ['article' => $article]) }}">
+                                        <i class="fas fa-pem mr-1"></i>記事をを更新する
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item text-danger" data-toggle="modal"
+                                        data-target="#modal-delete-{{ $article->id }}">
+                                        <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                </div>
                             </div>
                         </div>
+
+                        <!--modal-->
+                        <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <div class="modal-body">
+                                            {{ $article->title }}を削除します。よろしいですか？
+                                        </div>
+                                        <div class="modal-footer justify-center-between">
+                                            <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                                            <button type="submit" class="btn btn-danger">削除する</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!--modal-->
                     @endif
                 </div>
                 <div class="card-body pt-0 pb-2">
