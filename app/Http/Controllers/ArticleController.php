ce4e2f31ef89c08eae400b2f130d0681196a5662
ts44gj <ts44gj2020@gmail.com>
2021-09-09 13:13:22 +0900

    create

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,22 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\ArticleRequest;
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
-    public function index(){
-        $articles = Article::all()->sortByDesc('created_at');
-        return view('articles.index',['articles' => $articles]);
+     public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
     }
 
-    public function create(){
+    public function index()
+    {
+        $articles = Article::all()->sortByDesc('created_at');
+        return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
         return view('articles.create');
     }
 
-    public function store(ArticleRequest $request, Article $article){
+    public function store(ArticleRequest $request, Article $article)
+    {
         $article->title = $request->title;
         $article->body = $request->body;
         $article->user_id = $request->user()->id;
@@ -26,10 +33,26 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
-    public function edit(Article $article){
-
-        return view('articles.edit',['article' => $article]);
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
     }
 
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }
 
 }
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -2,25 +2,25 @@
 
 @section('title', '日記更新')
 
-@include('nav')
+    @include('nav')
 
 @section('content')
- <div class="container">
-     <div class='row'>
-         <div class='col-12'>
-             <div class="card mt-3">
-                 <div class='card-body pt-0'>
-                     @include('error_card_list')
-                     <div class='card-text'>
-                     <form method='POST' actiton="{{ route('articles.update',['article'=>$article]) }}">
-                        @method('PATCH')
-                        @include('articles.form')
-                        <button type="submit" class="btn blue-gradient btn-block">更新する</button>
-                     </form>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
+    <div class="container">
+        <div class='row'>
+            <div class='col-12'>
+                <div class="card mt-3">
+                    <div class='card-body pt-0'>
+                        @include('error_card_list')
+                        <div class='card-text'>
+                           <form method="POST" action="{{ route('articles.update', ['article' => $article]) }}">
+                                @method('PATCH')
+                                @include('articles.form')
+                                <button type="submit" class="btn blue-gradient btn-block">更新する</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
